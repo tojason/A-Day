c480e5df5e80648c78accc8fba84a06bba9cd118
Jason Situ <junsitu2@illinois.edu>
2017-12-29 12:13:24 -0600

    fix Semantic-UI not loaded

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+// In order for Semantic-UI to work,
+// the following line is mandatory
+import 'semantic-ui-css/semantic.min.css';
+
 import App from './components/App/App.jsx';
 
 require('./styles/main.scss');
